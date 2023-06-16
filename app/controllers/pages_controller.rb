f5d64c2a74665a6014207db2a349be7e88ef5644
Jazmin Orozco <jazmin.orozco@gmail.com>
2023-06-15 19:24:10 -0600

    Longest Word Game

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,2 +1,32 @@
+require "open-uri"
+
 class PagesController < ApplicationController
+  def new
+    alphabet = ('A'..'Z').to_a
+    # vowels = ['A', 'E', 'I', 'O', 'U', 'Y']
+    vowels = %w[A E I O U Y]
+    consonants = alphabet - vowels
+    @letters = Array.new(5) { consonants.sample }
+    @letters += Array.new(5) { vowels.sample }
+    @letters.shuffle!
+  end
+
+  def score
+    @letters = params[:letters].split
+    @word = (params[:word] || '').upcase
+    @included = included?(@word, @letters)
+    @english_word = english_word?(@word)
+  end
+
+  private
+
+  def included?(word, letters)
+    word.chars.all? { |letter| word.count(letter) <= letters.count(letter) }
+  end
+
+  def english_word?(word)
+    response = URI.open("https://wagon-dictionary.herokuapp.com/#{word}")
+    json = JSON.parse(response.read)
+    json['found']
+  end
 end
